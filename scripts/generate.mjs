ae6966773bec49f7c3f12b03039441fa3f1baa77
Al Harris <alharris@amazon.com>
2023-06-16 13:57:30 -0700

    updating generate script

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,12 +1,11 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as process from 'process';
-import { CloudFormationClient, DescribeStacksCommand } from '@aws-sdk/client-cloudformation';
-import { AppSyncClient, GetIntrospectionSchemaCommand, OutputType } from '@aws-sdk/client-appsync';
-import { spawn } from 'child_process';
+import * as cfn from '@aws-sdk/client-cloudformation';
+import * as appsync from '@aws-sdk/client-appsync';
+import * as pty from 'child_process';
 
 const schemaJsonPath = path.join(process.cwd(), 'schema.json');
-const appConfigPath = path.join(process.cwd(), 'src', 'appConfig.ts');
 
 const writeAppConfig = ({ apiKey, graphqlUrl }) => {
   const template = `const appConfig = {
@@ -17,11 +16,36 @@ const writeAppConfig = ({ apiKey, graphqlUrl }) => {
     
   export default appConfig;
   `;
-  fs.writeFileSync(appConfigPath, template);
+  fs.writeFileSync(path.join(process.cwd(), 'src', 'appConfig.ts'), template);
+};
+
+const writeGraphQLConfig = (apiId) => {
+  const template = `projects:
+  Codegen Project:
+    schemaPath: schema.json
+    includes:
+      - src/graphql/**/*.ts
+    excludes:
+      - ./amplify/**
+    extensions:
+      amplify:
+        codeGenTarget: typescript
+        generatedFileName: src/API.ts
+        docsFilePath: src/graphql
+        region: us-east-1
+        apiId: ${apiId}
+        frontend: javascript
+        framework: react
+        maxDepth: 2
+extensions:
+  amplify:
+    version: 3
+`;
+fs.writeFileSync(path.join(process.cwd(), '.graphqlconfig.yml'), template);
 };
 
 const getApiConfig = async () => {
-  const results = await new CloudFormationClient().send(new DescribeStacksCommand({}));
+  const results = await new cfn.CloudFormationClient().send(new cfn.DescribeStacksCommand({}));
   const stack = results.Stacks.filter(stack => stack.StackName === 'TestStack')[0];
   const apiKeyOutputs = stack.Outputs.filter(output => output.OutputKey === 'GraphQLAPIKeyOutput');
   const graphqlurlOutputs = stack.Outputs.filter(output => output.OutputKey === 'GraphQLAPIEndpointOutput');
@@ -34,9 +58,9 @@ const getApiConfig = async () => {
 };
 
 const downloadInstrospectionSchema = async (apiId) => {
-  const results = await new AppSyncClient().send(new GetIntrospectionSchemaCommand({
+  const results = await new appsync.AppSyncClient().send(new appsync.GetIntrospectionSchemaCommand({
     apiId,
-    format: OutputType.JSON,
+    format: appsync.OutputType.JSON,
     includeDirectives: false,
   }));
   if (!results.schema) {
@@ -45,8 +69,8 @@ const downloadInstrospectionSchema = async (apiId) => {
   fs.writeFileSync(schemaJsonPath, results.schema);
 };
 
-const invokeCodegen = async (apiId) => {
-  const codegen = spawn('amplify', ['codegen', '--apiId', apiId]);
+const invokeCodegen = (apiId) => new Promise((resolve) => {
+  const codegen = pty.spawn('amplify', ['codegen', '--apiId', ['codegen', '--apiId', apiId]]);
 
   codegen.stdout.on('data', (data) => {
     console.info(`codegen: ${data}`);
@@ -58,27 +82,32 @@ const invokeCodegen = async (apiId) => {
 
   codegen.on('close', (code) => {
     console.log(`child process close all stdio with code ${code}`);
+    resolve();
   });
   
   codegen.on('exit', (code) => {
     console.log(`child process exited with code ${code}`);
+    resolve();
   });
 
   codegen.on('disconnect', () => {
     console.log(`child process disconnected`);
+    resolve();
   });
-};
+});
 
 const generate = async () => {
   try {
-    console.log('[ 1 / 4 ] Pulling Api Config');
+    console.log('[ 1 / 5 ] Pulling Api Config');
     const { apiId, graphqlUrl, apiKey } = await getApiConfig();
-    console.log('[ 2 / 4 ] Downloading Introspection Schema');
+    console.log('[ 2 / 5 ] Downloading Introspection Schema');
     await downloadInstrospectionSchema(apiId);
-    console.log('[ 3 / 4 ] Invoking Amplify Codegen');
+    console.log('[ 3 / 5 ] Generating GraphQL Config');
+    writeGraphQLConfig(apiId)
+    console.log('[ 4 / 5 ] Invoking Amplify Codegen');
     await invokeCodegen(apiId);
-    console.log('[ 4 / 4 ] Writing App Config');
-    await writeAppConfig({ graphqlUrl, apiKey })
+    console.log('[ 5 / 5 ] Writing App Config');
+    writeAppConfig({ graphqlUrl, apiKey })
     process.exit(0);
   } catch (e) {
     console.error('Caught exception while executing generate script', e);
